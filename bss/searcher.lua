918740d0ee9a8ddb52f85dc9e18e3267fa16d33c
Liam nil <81304588+Disqualifi3d@users.noreply.github.com>
2026-02-05 17:29:31 +1100

    Update searcher.lua

--- bss/searcher.lua
+++ bss/searcher.lua
@@ -17,10 +17,11 @@ local found = false
 local Done = false
 
 local Settings = {
-    Threshold = 10,
+    Duration = 5
 }
 
-queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Disqualifi3d/Exp/refs/heads/main/bss/player.lua'))()")
+
+queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Disqualifi3d/Exp/refs/heads/main/bss/searcher.lua'))()")
 
 local s, e = pcall(function() 
 readfile("BSSLIST.DQ")
@@ -64,78 +65,14 @@ if readfile and not s then
 
 end
 
-local function getServerList(placeId)
-    local cursor
-    local servers = {}
-    repeat
-        local response = 
-            httprequest(
-                {
-                    Url = "https://games.roblox.com/v1/games/" .. placeId .. "/servers/Public?sortOrder=Asc&limit=100" .. (cursor and "&cursor=" .. cursor or ""),
-                    Method = "GET"
-            }
-        )
-    
-        for _, v in pairs(response) do
-            table.insert(servers, v)
-        end
-        cursor = response.nextPageCursor
-    until not cursor
-    return servers
-end
 
-local function DownloadServerList()
-    local Success, Error
+--[[
+BSSLIST.DQ -> {}.List -> {{id = 1}, {id = 1}}
+VICIOUSLIST.DQ -> {}.List -> {{id = 1}, {id = 1}}
+]]
 
-    repeat
-        Success, Error = pcall(function()
-            local Servers = getServerList(1537690962)
-
-            local Encoded_List = Servers[4]
-            local List = HttpService:JSONDecode(Encoded_List) and HttpService:JSONDecode(Encoded_List).data
-
-            local Downloaded = {
-                Last_Download = tick(),
-                List = List
-            }
-
-            local Encoded_File = HttpService:JSONEncode(Downloaded)
-
-            if Encoded_File and writefile then
-                writefile("BSSLIST.DQ", Encoded_File)
-            end
-        end)
-
-        if not Success then warn(Error) end
-
-        task.wait(10)
-    until Success
-end
-
-local function CheckServerAmount()
-    
+local function Get_New_Server()
     local file = readfile and readfile("BSSLIST.DQ")
-
-    if file then
-        local Decoded = HttpService:JSONDecode(file)
-
-        if Decoded.List then return #Decoded.List end
-    end
-
-end
-
-local function Check_Vicious_Bee_List()
-    local file = readfile and readfile("VICIOUSLIST.DQ")
-
-    if file then
-        local Decoded = HttpService:JSONDecode(file)
-
-        if Decoded.List then return #Decoded.List end
-    end
-end
-
-local function Get_Vicious_Bee_Server()
-    local file = readfile and readfile("VICIOUSLIST.DQ")
 
     if file then
         local Decoded = HttpService:JSONDecode(file)
@@ -145,56 +82,62 @@ local function Get_Vicious_Bee_Server()
         table.remove(Decoded.List, Selected_Server_Index)
 
         if writefile then
-            local New_File = HttpService:JSONEncode(Decoded)
+            local New_File = HttpService:JSONEncode(Decoded.List)
 
-            writefile("VICIOUSLIST.DQ", New_File)
+            writefile("BSSLIST.DQ", New_File)
         end
 
         if Selected and Selected.id then return Selected.id end
     end
 end
 
-while task.wait() do
-    
+local function Insert_Server()
+    local file = readfile and readfile("VICIOUSLIST.DQ")
+
+    if not file and readfile then
+        local Decoded = {
+            List = {}
+        }
+        local Encoded = HttpService:JSONEncode(Decoded)
+        if writefile then
+            writefile("VICIOUSLIST.DQ", Encoded)
+        end
+    end
+
+    local file = readfile and readfile("VICIOUSLIST.DQ")
+
+    if file then
+        local Decoded = HttpService:JSONDecode(file)
+
+        local Data = {id = game.JobId}
+
+        table.insert(Decoded.List, Data)
+
+        if writefile then
+            local New_File = HttpService:JSONEncode(Decoded.List)
+
+            writefile("VICIOUSLIST.DQ", New_File)
+        end
+    end
+end
+
+local start = tick()
+while task.wait() and start + Settings.Duration > tick() and Done == false do
     local WaitingThorn = WTs:FindFirstChild("WaitingThorn")
     local Vicious = Particles:FindFirstChild("Vicious")
 
     if (WaitingThorn or Vicious) and found == false then
-        found = true
-        
-        task.spawn(function()
-            task.spawn(function()
-                while task.wait(1) do
-                    if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
-                        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 80
-                    end
-                end
-            end)
-            loadstring(game:HttpGet("https://rawscripts.net/raw/Bee-Swarm-Simulator-KEYLESS-ATLAS-HUB-BEST-SCRIPT-FARMING-BEE-SWARM-84013"))()
-        end)
-        
-        if WaitingThorn then repeat print("waiting for thorn") task.wait() until not WaitingThorn or not WaitingThorn.Parent end
-        
-        local Vicious = Particles:WaitForChild("Vicious", 5)
-
-        if Vicious then repeat print("waiting for bee") task.wait() until not Vicious or not Vicious.Parent end
+        Insert_Server()
+        Done = true
     end
+end
 
-    if Check_Vicious_Bee_List() > 0 then
-        
-        local id = Get_Vicious_Bee_Server()
+while true do
+    
+    local New, Err = pcall(function()
+        local id = Get_New_Server()
+        Teleport_Service:TeleportToPlaceInstance(1537690962, id, game.Players.LocalPlayer)
+    end)
 
-        local New, Err = pcall(function()
-            Teleport_Service:TeleportToPlaceInstance(1537690962, id, game.Players.LocalPlayer)
-        end)
-
-        if New then print("Player should be teleporting") end
-
-        task.wait(10)
-    end
-
-    if CheckServerAmount() <= Settings.Threshold then
-        DownloadServerList()
-    end
-
+    task.wait(15)
 end
